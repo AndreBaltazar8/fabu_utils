438bdc7cc387cfcec43869ca29498604c91f3c19
André Baltazar <me@andrebaltazar.com>
2021-11-23 14:48:52 +0000

    feat: change nullable string extensions to fields

--- lib/string_extension.dart
+++ lib/string_extension.dart
@@ -5,8 +5,8 @@ import 'package:fabu_utils/dynamic_extension.dart';
 /// Contains common functionality used against nullable strings.
 extension NullableStringExtension on String? {
   /// Whether this string is null or empty.
-  bool isNullOrEmpty() => let((it) => it == null || it.isEmpty);
+  bool get isNullOrEmpty => let((it) => it == null || it.isEmpty);
 
   /// Whether this string is null or whitespace.
-  bool isNullOrWhitespace() => let((it) => it == null || it.trim().isEmpty);
+  bool get isNullOrWhitespace => let((it) => it == null || it.trim().isEmpty);
 }
